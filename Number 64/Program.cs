b1d4c493eb5a0c5d05d0f34994d8347e1890be9e
Vadim <orc225@mail.ru>
2023-02-14 23:10:54 +0300

    initial commit

---
+++ Number 64/Program.cs
@@ -0,0 +1,17 @@
+﻿//64
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+void Count(int N)
+{
+    if (N == 1)
+    {
+        Console.Write($"{N} "); //Делаем от 1 до N, так как от N до 1 
+                                // разобрано на семинаре
+    }
+    else
+    {
+        Count(N - 1);
+        Console.Write($"{N} ");
+    }
+}
+Count(N);
